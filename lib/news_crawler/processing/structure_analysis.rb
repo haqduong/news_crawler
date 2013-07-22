e97e1b9dc0a84314d097c3a1e3860a93913a0a81
Duong Quang Ha <contact@haqduong.net>
2013-07-22 17:59:28 +0700

    Bump version to 0.0.4-pre.1

--- lib/news_crawler/processing/structure_analysis.rb
+++ lib/news_crawler/processing/structure_analysis.rb
@@ -21,8 +21,9 @@
 #++
 
 require 'nokogiri'
+require 'uri'
 
-require 'news_crawler'
+require 'news_crawler/url_helper'
 require 'news_crawler/storage/url_queue'
 require 'news_crawler/storage/raw_data'
 require 'news_crawler/crawler_module'
@@ -33,6 +34,7 @@ module NewsCrawler
     # Database should only contains raw data from one website.
     class StructureAnalysis
       include CrawlerModule
+      include URLHelper
 
       def initialize
         @url_stats = {}
@@ -43,9 +45,37 @@ module NewsCrawler
 
       # Get and analyse url for information
       def analyse(url)
+#        puts "processing #{url}"
         html_doc = RawData.find_by_url(url)
         doc = Nokogiri.HTML(html_doc)
-        doc.xpath('//a')
+        inner_url = doc.xpath('//a').collect { | a_el |
+          temp_url = (a_el.attribute 'href').to_s
+          if (!temp_url.nil?) && (temp_url[0] == '/')
+            temp_url = URI.join(url, temp_url).to_s
+          end
+          temp_url
+        }
+
+        inner_url.delete_if { | url_0 |
+          (url_0.nil?) || (url_0.size == 0) || (url_0 == '#') ||
+          (url_0 == 'javascript:;')
+        }
+
+        inner_url.each do  | url |
+          @url_stats[url] = (@url_stats[url] || 0) + 1
+        end
+        mark_processed(url)
+      end
+
+      # Check if it is really 'url'
+      # @param [ String ] url
+      # @return [ Boolean ]
+      def is_url?(url)
+        (url.size != 0) && (url != '#') && (url != 'javascript:;')
+      end
+
+      def get_result
+        @url_stats
       end
     end
   end
